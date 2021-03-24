b40c04c263c3303242f2ec96edaf9f0d4956ce9e
Muhammad Alabadsa <moh22389@gmail.com>
2021-03-24 12:46:53 +0200

    Implement delete user Relates #7

--- server/controller/routes/user/deleteUser.js
+++ server/controller/routes/user/deleteUser.js
@@ -1,8 +1,9 @@
 const { deleteUser } = require('../../../database/queries/user');
+const User = require('../../../database/models/users');
 
 const deleteUserController = async (req, res, next) => {
   try {
-    await deleteUser(req.userId);
+    await User.findByIdAndDelete(req.userId);
     return res.clearCookie('token').json({
       statusCode: 200,
       message: 'Account deleted successfully',
